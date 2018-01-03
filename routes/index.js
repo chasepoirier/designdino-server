ceed5d3f12f95d8d1b2da7fe1a2e719d464a44ec
Chase Poirier <chase.n.poirier@gmail.com>
2018-01-03 10:46:17 -0800

    push to heroku

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+
+	res.send('welcome to the design dino api');
+});
+
 router.get('/customers', function(req, res, next) {
   const customers = [
 		{
