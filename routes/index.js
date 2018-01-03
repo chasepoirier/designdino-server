7acce741d28930d508bb20bb1172b0874434e0de
Chase Poirier <chase.n.poirier@gmail.com>
2018-01-02 23:08:49 -0800

    push backend to heroku

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/customers', function(req, res, next) {
+  const customers = [
+		{
+			id: 1,
+			firstName: "Chase",
+			lastName: 'Poirier'
+		},
+		{
+			id: 2,
+			firstName: "Madison",
+			lastName: "Yocum"
+		},
+		{
+			id: 3,
+			firstName: "Ishan",
+			lastName: "Chhabra"
+		}
+	];
+
+	res.json(customers);
+});
+
+module.exports = router;
