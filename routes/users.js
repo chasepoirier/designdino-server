6c4cdf84a5b016daf783da485e5f2c11b2f09d70
Chase Poirier <chase.n.poirier@gmail.com>
2018-01-17 14:08:33 -0500

    updated

--- routes/users.js
+++ routes/users.js
@@ -23,11 +23,11 @@ const router = express.Router();
 
 router.post("/", (req, res) => {
   	const { email, password, username, name } = req.body.user;
- //  	const passwordHash = password;
- //  	const user = new User({ name, email, username, passwordHash });
+  	const passwordHash = password;
+  	const user = new User({ name, email, username, passwordHash });
 
 	// user.setConfirmationToken();
-	// user.save();
+	user.save();
 
 	res.json({ user: {username: username}})
   	// user.save().then(userRecord => {
