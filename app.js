e3ed9034553c2b7e2107787f0bc0e62610bf9643
Chase Poirier <chase.n.poirier@gmail.com>
2018-01-17 13:54:42 -0500

    updated

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500).json(err);
+  res.status(err.status || 500).json({err: err});
   console.log(err);
   res.send(err);
 });
